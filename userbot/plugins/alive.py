a6ef81e6e256c4d9acda575d0fa7c24b5e1538f2
shantanunautiyal <56751285+shantanunautiyal@users.noreply.github.com>
2020-06-08 20:57:29 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,4 +1,4 @@
-"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+"""Check if userbot alive."""
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -13,19 +13,19 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**MY STATUS** \n`FRIDAY IS:` **✅ Alive**\n\n"
+    await alive.edit("**MY STATUS** \n`Shantanu's Bot IS:` **✅ Alive**\n\n"
                      "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
                      "`DATABASE STATUS:` **ALL NORMAL! WORKING FINE 🙂**\n`NO PROBLEM FOUND🔥!\n`"
                      "`CONNECTION PROVIDER:` ** AMAZON WEB SERVICES\n"
-                     "`CURRECT BOT LOCATION:` ** ASHBURN,UNITED STATES\n"
+                     "`CURRECT BOT LOCATION:` ** Delhi,India\n"
                      "`DEPLOYING SERVICE BY: HEROKU INC.\n"
                      "`IP:` 3.82.66.232\n"
                      "`SYSTEM TYPE:` LINUX\n"
                      "`BASED ON:` UNIBORG\n"
-                     "`SATELLITE:` ⚡STARK SAT-2⚡\n"
+                     "`SATELLITE:` ⚡SHANTANU SAT-1⚡\n"
                      "`MADE USING:` PYTHON\n"
                      f"`MY BOSS`: {DEFAULTUSER}\n\n"
                      "ALWAYS WITH YOU MY BOSS\n\n"
                      "`SATELLITE STATUS: ✅ Alive\n\n"
-                     "[⚡DEPLOY FRIDAY⚡](https://github.com/StarkGang/FRIDAYUserbot)") 
+                     "[⚡DEPLOY SHANTANU⚡]") 
 
